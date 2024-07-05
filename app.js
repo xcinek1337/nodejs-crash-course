a8f0020497c354e1ebc57b674c56e6d3ccdbb0a3
Marcin <marcinchodkowski004@wp.pl>
2024-07-05 14:07:35 +0200

    handling with db data

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+const dbURI =
+	'mongodb+srv://marcinchodkowski555:qwerty123@node-js.jumllfk.mongodb.net/node-js?retryWrites=true&w=majority&appName=node-js';
+mongoose
+	.connect(dbURI)
+	.then((result) => app.listen(3000))
+	.catch((err) => console.log(err));
 // listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// mongoose and mongo sanbox routes
+app.get('/add-blog', (req, res) => {
+	const blog = new Blog({
+		title: 'new blog 2',
+		snippet: 'about my new blog',
+		body: 'more about my new blog',
+	});
+	blog
+		.save()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {
+	Blog.find()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+app.get('/single-blog', (req, res) => {
+	Blog.findById('6687e01f6c9aa733c04c8dcc')
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
 
 // register view engine
 app.set('view engine', 'ejs');
